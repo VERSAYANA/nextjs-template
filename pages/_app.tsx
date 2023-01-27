d4f67d9f05fe57dad8c07d718b665eebf93bf420
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-01-27 05:32:10 +0330

    Add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,47 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Next.js template with tailwindcss & prettier</title>
+        <meta
+          name="description"
+          content="Next.js template with tailwindcss & prettier"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Open Graph */}
+        <meta
+          property="og:title"
+          content="Next.js template with tailwindcss & prettier"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          name="description"
+          content="Next.js template with tailwindcss & prettier"
+        />
+        <meta property="og:image" content="/og-image.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        {/* Twitter */}
+        <meta
+          property="twitter:title"
+          content="Next.js template with tailwindcss & prettier"
+        />
+        <meta
+          property="twitter:description"
+          content="Next.js template with tailwindcss & prettier"
+        />
+      </Head>
+      <div className={`${inter.className}`}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
